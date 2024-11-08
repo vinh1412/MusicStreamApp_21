175f414f077c70e640e3bec84a9c87552e32fbce
Duong-GB <duongnguyenqn1323@gmail.com>
2024-11-08 14:13:58 +0700

    [UPD] Cập nhật BottomTabNavigator để hiển thị màn hình MyLibraryScreen

--- components/navigators/BottomTabNavigator.js
+++ components/navigators/BottomTabNavigator.js
@@ -5,6 +5,7 @@ import HomeScreen from "../screens/AudioListingScreen";
 import SearchScreen from "../screens/SearchResultScreen";
 import FeedAudioListingScreen from "../screens/FeedAudioListingScreen";
 import LibraryScreen from "../screens/AudioListingScreen";
+import MyLibraryScreen from "../screens/MyLibraryScreen";
 import PlaylistDetailScreen from "../screens/PlaylistDetailScreen";
 import ArtistProfileScreen from "../screens/ArtistProfileScreen";
 
@@ -12,75 +13,83 @@ import { Image, Text } from "react-native";
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
-const AudioStackNavigator = ({route}) => {
-    const {userName} = route.params;
-  return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="AudioListingScreen" component={HomeScreen} initialParams={{userName}} />
-      <Stack.Screen
-        name="PlaylistDetailScreen"
-        component={PlaylistDetailScreen}
-        options={{
-          animation: "slide_from_right",
-        }}
-      />
-      <Stack.Screen
-        name="ArtistProfileScreen"
-        component={ArtistProfileScreen}
-        options={{
-          animation: "slide_from_right",
-        }}
-      />
-    </Stack.Navigator>
-  );
+const AudioStackNavigator = ({ route }) => {
+    const { userName } = route.params;
+    return (
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen
+                name="AudioListingScreen"
+                component={HomeScreen}
+                initialParams={{ userName }}
+            />
+            <Stack.Screen
+                name="PlaylistDetailScreen"
+                component={PlaylistDetailScreen}
+                options={{
+                    animation: "slide_from_right",
+                }}
+            />
+            <Stack.Screen
+                name="ArtistProfileScreen"
+                component={ArtistProfileScreen}
+                options={{
+                    animation: "slide_from_right",
+                }}
+            />
+        </Stack.Navigator>
+    );
 };
 const BottomTabNavigator = ({ route }) => {
-  const { userName } = route.params;
-  return (
-    <Tab.Navigator
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ color, size }) => {
-          let iconPath;
+    const { userName } = route.params;
+    return (
+        <Tab.Navigator
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ color, size }) => {
+                    let iconPath;
 
-          // Cấu hình icon cho từng tab
-          if (route.name === "Home") {
-            iconPath = require("../../assets/icons/homeIcon.png");
-          } else if (route.name === "Search") {
-            iconPath = require("../../assets/icons/searchIcon.png");
-          } else if (route.name === "Feed") {
-            iconPath = require("../../assets/icons/feedIcon.png");
-          } else if (route.name === "Library") {
-            iconPath = require("../../assets/icons/libraryIcon.png");
-          }
+                    // Cấu hình icon cho từng tab
+                    if (route.name === "Home") {
+                        iconPath = require("../../assets/icons/homeIcon.png");
+                    } else if (route.name === "Search") {
+                        iconPath = require("../../assets/icons/searchIcon.png");
+                    } else if (route.name === "Feed") {
+                        iconPath = require("../../assets/icons/feedIcon.png");
+                    } else if (route.name === "Library") {
+                        iconPath = require("../../assets/icons/libraryIcon.png");
+                    }
 
-          return (
-            <Image
-              source={iconPath}
-              style={{
-                width: size,
-                height: size,
-                tintColor: color,
-              }}
+                    return (
+                        <Image
+                            source={iconPath}
+                            style={{
+                                width: size,
+                                height: size,
+                                tintColor: color,
+                            }}
+                        />
+                    );
+                },
+                tabBarLabel: ({ focused }) => (
+                    <Text style={{ color: focused ? "#2FC9DD" : "#8e8e93" }}>
+                        {route.name}
+                    </Text>
+                ),
+                tabBarActiveTintColor: "#2FC9DD",
+                tabBarInactiveTintColor: "#8e8e93",
+                headerShown: false,
+                tabBarStyle: { height: 100, paddingBottom: 20, paddingTop: 20 },
+            })}
+        >
+            <Tab.Screen
+                name="Home"
+                component={AudioStackNavigator}
+                initialParams={{ userName }}
             />
-          );
-        },
-        tabBarLabel: ({ focused }) => (
-          <Text style={{ color: focused ? "#2FC9DD" : "#8e8e93" }}>
-            {route.name}
-          </Text>
-        ),
-        tabBarActiveTintColor: "#2FC9DD",
-        tabBarInactiveTintColor: "#8e8e93",
-        headerShown: false,
-        tabBarStyle: { height: 100, paddingBottom: 20, paddingTop: 20 },
-      })}
-    >
-      <Tab.Screen name="Home" component={AudioStackNavigator} initialParams={{userName}}/>
-      <Tab.Screen name="Search" component={SearchScreen} />
-      <Tab.Screen name="Feed" component={FeedAudioListingScreen} />
-      <Tab.Screen name="Library" component={SearchScreen} />
-    </Tab.Navigator>
-  );
+            <Tab.Screen name="Search" component={SearchScreen} />
+            <Tab.Screen name="Feed" component={FeedAudioListingScreen} />
+            <Tab.Screen name="Library" component={MyLibraryScreen} />
+        </Tab.Navigator>
+    );
 };
 
 export default BottomTabNavigator;
--- components/screens/LoginScreen.js
+++ components/screens/LoginScreen.js
@@ -1,196 +1,201 @@
 // LoginScreen.js
 import React, { useState, useEffect } from "react";
 import {
-  View,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  Alert,
-  StyleSheet,
-  ImageBackground,
-  Dimensions,
+    View,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    Alert,
+    StyleSheet,
+    ImageBackground,
+    Dimensions,
 } from "react-native";
 import Icon from "react-native-vector-icons/Fontisto";
 import Icon1 from "react-native-vector-icons/Feather";
 const { width, height } = Dimensions.get("screen");
 const LoginScreen = ({ navigation }) => {
-  const [isPasswordVisible, setPasswordVisible] = useState(false);
-  // Show or hide password
-  const togglePasswordVisibility = () => {
-    setPasswordVisible(!isPasswordVisible);
-  };
-  const [users, setUsers] = useState([]);
-  // Read API
-  const readApi = async () => {
-    try {
-      const response = await fetch(
-        "https://6711cec24eca2acdb5f5d0cb.mockapi.io/api/users"
-      );
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      const data = await response.json();
-      setUsers(data);
-    } catch (error) {
-      console.error("There was a problem with the fetch operation:", error);
-      Alert.alert("Error", "Failed to load users.");
-    }
-  };
+    const [isPasswordVisible, setPasswordVisible] = useState(false);
+    // Show or hide password
+    const togglePasswordVisibility = () => {
+        setPasswordVisible(!isPasswordVisible);
+    };
+    const [users, setUsers] = useState([]);
+    // Read API
+    const readApi = async () => {
+        try {
+            const response = await fetch(
+                "https://6711cec24eca2acdb5f5d0cb.mockapi.io/api/users"
+            );
+            if (!response.ok) {
+                throw new Error("Network response was not ok");
+            }
+            const data = await response.json();
+            setUsers(data);
+        } catch (error) {
+            console.error(
+                "There was a problem with the fetch operation:",
+                error
+            );
+            Alert.alert("Error", "Failed to load users.");
+        }
+    };
 
-  useEffect(() => {
-    readApi();
-  }, []);
+    useEffect(() => {
+        readApi();
+    }, []);
 
-  const [email, setEmail] = useState("vinh@gmail.com");
-  const [password, setPassword] = useState("123");
-  // Handle login
-  const handleLogin = () => {
-    const isEmptyField = email === "" || password === "";
-    if (isEmptyField) {
-      alert("Please fill all fields");
-    } else {
-      const user = users.find(
-        (user) => user.email === email && user.password === password
-      );
-      if (user) {
-        navigation.navigate("BottomTabNavigator", { userName: user.name });
-        setEmail("");
-        setPassword("");
-      } else {
-        alert("Invalid email or password");
-      }
-    }
-  };
+    const [email, setEmail] = useState("duong@gmail.com");
+    const [password, setPassword] = useState("123");
+    // Handle login
+    const handleLogin = () => {
+        const isEmptyField = email === "" || password === "";
+        if (isEmptyField) {
+            alert("Please fill all fields");
+        } else {
+            const user = users.find(
+                (user) => user.email === email && user.password === password
+            );
+            if (user) {
+                navigation.navigate("BottomTabNavigator", {
+                    userName: user.name,
+                });
+                setEmail("");
+                setPassword("");
+            } else {
+                alert("Invalid email or password");
+            }
+        }
+    };
 
-  return (
-    <ImageBackground
-      source={require("../../assets/images/loginScreen/brLoginImage.png")}
-      style={styles.background}
-    >
-      <View style={styles.container}>
-        {/* Top view */}
-        <View style={styles.titleView}>
-          <Text style={styles.title}>Login</Text>
-        </View>
-        {/* Input */}
-        <View style={{ marginTop: 80, marginBottom: 50 }}>
-          <View style={styles.inputView}>
-            <Icon
-              name="email"
-              size={25}
-              color="#000"
-              style={styles.iconInput}
-            />
-            <TextInput
-              style={styles.input}
-              placeholder="Email"
-              value={email}
-              onChangeText={setEmail}
-            />
-          </View>
-          <View style={styles.inputView}>
-            <Icon1
-              name="lock"
-              size={25}
-              color="#000"
-              style={styles.iconInput}
-            />
-            <TextInput
-              style={styles.input}
-              placeholder="Password"
-              value={password}
-              onChangeText={setPassword}
-              secureTextEntry={!isPasswordVisible}
-            />
-            <Icon1
-              name={isPasswordVisible ? "eye" : "eye-off"}
-              size={25}
-              color="#000"
-              style={styles.iconEye}
-              onPress={togglePasswordVisibility}
-            />
-          </View>
-        </View>
-        {/* Login button */}
-        <TouchableOpacity
-          style={styles.loginButton}
-          onPress={() => handleLogin()}
+    return (
+        <ImageBackground
+            source={require("../../assets/images/loginScreen/brLoginImage.png")}
+            style={styles.background}
         >
-          <Text style={styles.loginText}>Login</Text>
-        </TouchableOpacity>
-        {/* Back button */}
-        <TouchableOpacity
-          style={styles.loginButtonBack}
-          onPress={() => navigation.goBack()}
-        >
-          <Text style={styles.loginText}>Back</Text>
-        </TouchableOpacity>
-      </View>
-    </ImageBackground>
-  );
+            <View style={styles.container}>
+                {/* Top view */}
+                <View style={styles.titleView}>
+                    <Text style={styles.title}>Login</Text>
+                </View>
+                {/* Input */}
+                <View style={{ marginTop: 80, marginBottom: 50 }}>
+                    <View style={styles.inputView}>
+                        <Icon
+                            name="email"
+                            size={25}
+                            color="#000"
+                            style={styles.iconInput}
+                        />
+                        <TextInput
+                            style={styles.input}
+                            placeholder="Email"
+                            value={email}
+                            onChangeText={setEmail}
+                        />
+                    </View>
+                    <View style={styles.inputView}>
+                        <Icon1
+                            name="lock"
+                            size={25}
+                            color="#000"
+                            style={styles.iconInput}
+                        />
+                        <TextInput
+                            style={styles.input}
+                            placeholder="Password"
+                            value={password}
+                            onChangeText={setPassword}
+                            secureTextEntry={!isPasswordVisible}
+                        />
+                        <Icon1
+                            name={isPasswordVisible ? "eye" : "eye-off"}
+                            size={25}
+                            color="#000"
+                            style={styles.iconEye}
+                            onPress={togglePasswordVisibility}
+                        />
+                    </View>
+                </View>
+                {/* Login button */}
+                <TouchableOpacity
+                    style={styles.loginButton}
+                    onPress={() => handleLogin()}
+                >
+                    <Text style={styles.loginText}>Login</Text>
+                </TouchableOpacity>
+                {/* Back button */}
+                <TouchableOpacity
+                    style={styles.loginButtonBack}
+                    onPress={() => navigation.goBack()}
+                >
+                    <Text style={styles.loginText}>Back</Text>
+                </TouchableOpacity>
+            </View>
+        </ImageBackground>
+    );
 };
 
 const styles = StyleSheet.create({
-  background: {
-    flex: 1,
-    width: width,
-    height: height,
-  },
-  container: {
-    justifyContent: "center",
-    paddingHorizontal: 20,
-  },
-  titleView: {
-    marginVertical: 100,
-  },
-  title: {
-    fontSize: 60,
-    fontWeight: "bold",
-    marginBottom: 20,
-    textAlign: "center",
-    color: "white",
-  },
-  inputView: {
-    flexDirection: "row",
-    alignItems: "center",
-    borderRadius: 25,
-    marginBottom: 20,
-    paddingHorizontal: 10,
-    backgroundColor: "#fff",
-    width: "100%",
-  },
-  iconInput: {
-    paddingHorizontal: 10,
-  },
-  input: {
-    paddingVertical: 10,
-    paddingHorizontal: 10,
-    fontSize: 16,
-    width: "80%",
-  },
-  iconEye: {
-    right: 0,
-  },
-  loginButton: {
-    backgroundColor: "#25C3D9",
-    paddingVertical: 15,
-    paddingHorizontal: 100,
-    borderRadius: 25,
-    marginBottom: 15,
-  },
-  loginButtonBack: {
-    backgroundColor: "red",
-    paddingVertical: 15,
-    paddingHorizontal: 100,
-    borderRadius: 30,
-    marginBottom: 15,
-  },
-  loginText: {
-    color: "#fff",
-    fontSize: 16,
-    textAlign: "center",
-    fontWeight: "bold",
-  },
+    background: {
+        flex: 1,
+        width: width,
+        height: height,
+    },
+    container: {
+        justifyContent: "center",
+        paddingHorizontal: 20,
+    },
+    titleView: {
+        marginVertical: 100,
+    },
+    title: {
+        fontSize: 60,
+        fontWeight: "bold",
+        marginBottom: 20,
+        textAlign: "center",
+        color: "white",
+    },
+    inputView: {
+        flexDirection: "row",
+        alignItems: "center",
+        borderRadius: 25,
+        marginBottom: 20,
+        paddingHorizontal: 10,
+        backgroundColor: "#fff",
+        width: "100%",
+    },
+    iconInput: {
+        paddingHorizontal: 10,
+    },
+    input: {
+        paddingVertical: 10,
+        paddingHorizontal: 10,
+        fontSize: 16,
+        width: "80%",
+    },
+    iconEye: {
+        right: 0,
+    },
+    loginButton: {
+        backgroundColor: "#25C3D9",
+        paddingVertical: 15,
+        paddingHorizontal: 100,
+        borderRadius: 25,
+        marginBottom: 15,
+    },
+    loginButtonBack: {
+        backgroundColor: "red",
+        paddingVertical: 15,
+        paddingHorizontal: 100,
+        borderRadius: 30,
+        marginBottom: 15,
+    },
+    loginText: {
+        color: "#fff",
+        fontSize: 16,
+        textAlign: "center",
+        fontWeight: "bold",
+    },
 });
 
 export default LoginScreen;
