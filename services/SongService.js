3d27fbe2b2628a51f7c87af05b91bb61d8e39052
Tran Hien Vinh <tranvinh141203@gmail.com>
2024-11-21 01:30:34 +0700

    [UPD] Cập nhật services.SongServie và services.UserService để lấy data từ api

--- services/SongService.js
+++ services/SongService.js
@@ -1,3 +1,3 @@
 import axios from "axios";
-const REST_API_URL = "http://192.168.1.91:8080/api/songs";
+const REST_API_URL = "http://192.168.1.61:8080/api/songs";
 export const listSongs = () => axios.get(REST_API_URL);
