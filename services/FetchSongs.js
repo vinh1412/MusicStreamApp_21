dba1df11a6a9ef9e0ae883f539f395527a2db87e
Duong-GB <duongnguyenqn1323@gmail.com>
2024-12-02 11:49:02 +0700

    [UPD] Cập nhật api để chạy

--- services/FetchSongs.js
+++ services/FetchSongs.js
@@ -3,26 +3,29 @@ import { useAudio } from "../components/context/AudioContext";
 import { listSongs } from "../services/SongService"; // Gọi API
 
 const FetchSongs = () => {
-  const { setSongs } = useAudio();
+    const { setSongs } = useAudio();
 
-  useEffect(() => {
-    const fetchSongs = async () => {
-      try {
-        const response = await listSongs(); // Gọi API để lấy danh sách bài hát
-        if (response.status === 200) {
-          setSongs(response.data); // Lưu dữ liệu vào context
-        } else {
-          console.error("Failed to fetch songs. Status:", response.status);
-        }
-      } catch (error) {
-        console.error("Error fetching songs:", error.message);
-      }
-    };
+    useEffect(() => {
+        const fetchSongs = async () => {
+            try {
+                const response = await listSongs(); // Gọi API để lấy danh sách bài hát
+                if (response.status === 200) {
+                    setSongs(response.data); // Lưu dữ liệu vào context
+                } else {
+                    console.error(
+                        "Failed to fetch songs. Status:",
+                        response.status
+                    );
+                }
+            } catch (error) {
+                console.error("Error fetching songs:", error.message);
+            }
+        };
 
-    fetchSongs();
-  }, [setSongs]);
+        fetchSongs();
+    }, [setSongs]);
 
-  return null; // Không render gì
+    return null; // Không render gì
 };
 
 export default FetchSongs;
